67e8a5359e48d1d1c7abf7d35468bc73c2de1d5d
David Neumann <david.neumann@lecode.de>
2016-01-21 16:03:07 +0100

    Started to work on the view models for the sample application.

--- System.Windows.Mvvm.Sample/App.xaml.cs
+++ System.Windows.Mvvm.Sample/App.xaml.cs
@@ -1,7 +1,9 @@
 ﻿
 #region Using Directives
 
+using System.Threading.Tasks;
 using System.Windows.Mvvm.Application;
+using System.Windows.Mvvm.Sample.Repositories;
 
 #endregion
 
@@ -13,8 +15,19 @@ namespace System.Windows.Mvvm.Sample
     public partial class App : MvvmApplication
     {
         #region MvvmApplication Implementation
-        
-        
+
+        /// <summary>
+        /// This is the entry-piont to the application, which gets called as soon as the application has finished starting up.
+        /// </summary>
+        /// <param name="eventArguments">The event arguments, that contains all necessary information about the application startup like the command line arguments.</param>
+        protected override Task OnStartedAsync(ApplicationStartedEventArgs eventArguments)
+        {
+            // Binds the todo list item repository to the Ninject kernel, so that it can be automatically injected into view models
+            this.Kernel.Bind<TodoListItemsRepository>().ToSelf().InSingletonScope();
+
+            // Since no asynchronous operation was performed, an empty task is returned
+            return Task.FromResult(0);
+        }
 
         #endregion
     }
--- System.Windows.Mvvm.Sample/ViewModels/MainViewModel.cs
+++ System.Windows.Mvvm.Sample/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 #region Using Directives
 
 using System.Windows.Mvvm.Reactive;
+using System.Windows.Mvvm.Sample.Repositories;
 
 #endregion
 
@@ -12,5 +13,38 @@ namespace System.Windows.Mvvm.Sample.ViewModels
     /// </summary>
     public class MainViewModel : ReactiveViewModel
     {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new <see cref="MainViewModel"/> instance.
+        /// </summary>
+        /// <param name="todoListItemsRepository">The todo list items repository, which can be used to manage the items on the todo list.</param>
+        public MainViewModel(TodoListItemsRepository todoListItemsRepository)
+        {
+            this.todoListItemsRepository = todoListItemsRepository;
+        }
+
+        #endregion
+
+        #region Private Fields
+
+        /// <summary>
+        /// Contains the todo list items repository, which can be used to manage the items on the todo list.
+        /// </summary>
+        private readonly TodoListItemsRepository todoListItemsRepository;
+
+        #endregion
+
+        #region Public Properties
+
+
+
+        #endregion
+
+        #region ReactiveViewModel Implementation
+
+
+
+        #endregion
     }
 }
