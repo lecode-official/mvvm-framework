58620f93aaf0415fd970d7085eb383cdfc6e4b0c
David Neumann <david.neumann@live.com>
2016-02-04 20:22:23 +0100

    Bumped up the version number and made the project ready for publishing on NuGet.

--- Samples/MvvmFramework.Samples.Wpf/Properties/AssemblyInfo.cs
+++ Samples/MvvmFramework.Samples.Wpf/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Windows;
 
 #region Assembly Version Information
 
-[assembly: AssemblyVersion("0.1.8.*")]
-[assembly: AssemblyFileVersion("0.1.8.0")]
+[assembly: AssemblyVersion("0.2.0.*")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
 
 #endregion
