d0810c26e14a3d79e6d4beabf8eed75facb6c90e
David Neumann <david.neumann@lecode.de>
2016-03-19 12:40:18 +0100

    Bumbed up the version number and made the solution ready for release on NuGet.

--- Samples/MvvmFramework.Samples.Wpf/Properties/AssemblyInfo.cs
+++ Samples/MvvmFramework.Samples.Wpf/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Windows;
 
 #region Assembly Version Information
 
-[assembly: AssemblyVersion("0.2.1.*")]
-[assembly: AssemblyFileVersion("0.2.1.0")]
+[assembly: AssemblyVersion("0.2.2.*")]
+[assembly: AssemblyFileVersion("0.2.2.0")]
 
 #endregion
