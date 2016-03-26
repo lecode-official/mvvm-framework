4d530e2f596d184d072a13315921683f77e36e69
lukasroegner <lukas.roegner@outlook.com>
2016-03-26 13:59:58 +0100

    Added two new converters that check for type equality of the value and the converter parameter. The functionality is similar to the enum converters.

--- System.Windows.Mvvm.UI/Controls/EnumToBooleanConverter.cs
+++ System.Windows.Mvvm.UI/Controls/EnumToBooleanConverter.cs
@@ -16,36 +16,33 @@ namespace System.Windows.Controls
         #region IValueConverter Implementation
 
         /// <summary>
-        /// Convertes the value parameter to a boolean value.
+        /// Converts the value parameter to a boolean value.
         /// </summary>
         /// <param name="value">The value of an enum that is to be converted.</param>
         /// <param name="targetType">The type to which the value is to be converted. In this case it is always boolean.</param>
-        /// <param name="parameter">A paramter that determines the value to which the value has to be equal to return true.</param>
+        /// <param name="parameter">A parameter that determines the value to which the value has to be equal to return true.</param>
         /// <param name="culture">The culture information of the current culture, so that parsing can be adjusted to cultural conventions.</param>
-        /// <returns>Returns true if the value parameter has the same value as the paramter parameter.</returns>
+        /// <returns>Returns true if the value parameter has the same value as the parameter parameter.</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            // Check if the value provided is null, if so then tell the converter that the value is unset
-            if (value == null)
+            // Checks if the value provided is null, if so then tell the converter that the value is unset
+            if (value == null || !(value is Enum))
                 return DependencyProperty.UnsetValue;
 
-            // Get the parameter and check if it is null, if so then tell the converter, that the value is unset
+            // Gets the parameter and check if it is null, if so then tells the converter, that the value is unset
             string enumValueName = parameter as string;
             if (enumValueName == null)
                 return DependencyProperty.UnsetValue;
 
-            // Check if the enum type, from which is to be converted defines the value that is to be converted to boolean, if not, then tell the converter, that the value is unset
+            // Checks if the enum type, from which is to be converted defines the value that is to be converted to boolean, if not, then tell the converter, that the value is unset
             if (!Enum.IsDefined(value.GetType(), value))
                 return DependencyProperty.UnsetValue;
 
-            // Convert the parameter value to the enum type
+            // Converts the parameter value to the enum type
             object parameterEnumValue = Enum.Parse(value.GetType(), enumValueName);
 
-            // If the parameter equals the value that is to be converted, then return true, otherwise false is returned
-            if (parameterEnumValue.Equals(value))
-                return true;
-            else
-                return false;
+            // If the parameter equals the value that is to be converted, then returns true, otherwise false is returned
+            return parameterEnumValue.Equals(value);
         }
 
         /// <summary>
@@ -53,16 +50,16 @@ namespace System.Windows.Controls
         /// </summary>
         /// <param name="value">The value that is to be converted.</param>
         /// <param name="targetType">The type to which the value is to be converted. In this case it is an enum type.</param>
-        /// <param name="parameter">A paramter whose enum value is returned.</param>
+        /// <param name="parameter">A parameter whose enum value is returned.</param>
         /// <param name="culture">The culture information of the current culture, so that parsing can be adjusted to cultural conventions.</param>
         /// <returns>Returns the enum value of the parameter.</returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            // Check if the value provided is null, if so then tell the converter that the value is unset
+            // Checks if the value provided is null, if so then tell the converter that the value is unset
             if (value == null)
                 return DependencyProperty.UnsetValue;
 
-            // Get the parameter and check if it is null, if so then tell the converter, that the value is unset
+            // Gets the parameter and check if it is null, if so then tell the converter, that the value is unset
             string enumValueName = parameter as string;
             if (enumValueName == null)
                 return DependencyProperty.UnsetValue;
--- System.Windows.Mvvm.UI/Controls/TypeToVisibilityConverter.cs
+++ System.Windows.Mvvm.UI/Controls/TypeToVisibilityConverter.cs
@@ -9,16 +9,16 @@ using System.Windows.Data;
 namespace System.Windows.Controls
 {
     /// <summary>
-    /// Converts an enum to the visible value, if the parameter has the same value as the enum value.
+    /// Converts a type to the visible value, if the parameter has the same value as the type.
     /// </summary>
-    public class EnumToVisibilityConverter : IValueConverter
+    public class TypeToVisibilityConverter : IValueConverter
     {
         #region IValueConverter Implementation
 
         /// <summary>
         /// Converts the value parameter to a visibility value.
         /// </summary>
-        /// <param name="value">The value of an enum that is to be converted.</param>
+        /// <param name="value">The value that is to be converted.</param>
         /// <param name="targetType">The type to which the value is to be converted. In this case it is always visibility.</param>
         /// <param name="parameter">A parameter that determines the value to which the value has to be equal to return visible.</param>
         /// <param name="culture">The culture information of the current culture, so that parsing can be adjusted to cultural conventions.</param>
@@ -26,46 +26,44 @@ namespace System.Windows.Controls
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             // Checks if the value provided is null, if so then tell the converter that the value is unset
-            if (value == null || !(value is Enum))
+            if (value == null)
+                return DependencyProperty.UnsetValue;
+
+            // Gets the value as type and check if it is null, if so then tells the converter, that the value is unset
+            Type typeValue = value as Type;
+            if (typeValue == null)
                 return DependencyProperty.UnsetValue;
 
             // Gets the parameter and check if it is null, if so then tells the converter, that the value is unset
-            string enumValueName = parameter as string;
-            if (enumValueName == null)
+            Type parameterValue = parameter as Type;
+            if (parameterValue == null)
                 return DependencyProperty.UnsetValue;
-
-            // Checks if the enum type, from which is to be converted defines the value that is to be converted to visibility, if not, then tells the converter, that the value is unset
-            if (!Enum.IsDefined(value.GetType(), value))
-                return DependencyProperty.UnsetValue;
-
-            // Converts the parameter value to the enum type
-            object parameterEnumValue = Enum.Parse(value.GetType(), enumValueName);
 
             // If the parameter equals the value that is to be converted, then returns visible, otherwise hidden is returned
-            return parameterEnumValue.Equals(value) ? Visibility.Visible : Visibility.Hidden;
+            return typeValue == parameterValue ? Visibility.Visible : Visibility.Hidden;
         }
 
         /// <summary>
-        /// Converts a visibility value back to an enum value.
+        /// Converts a visibility value back to a type.
         /// </summary>
         /// <param name="value">The value that is to be converted.</param>
-        /// <param name="targetType">The type to which the value is to be converted. In this case it is an enum type.</param>
+        /// <param name="targetType">The type to which the value is to be converted. In this case it is type.</param>
         /// <param name="parameter">A parameter whose enum value is returned.</param>
         /// <param name="culture">The culture information of the current culture, so that parsing can be adjusted to cultural conventions.</param>
-        /// <returns>Returns the enum value of the parameter.</returns>
+        /// <returns>Returns the type of the parameter.</returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            // Checks if the value provided is null, if so then tells the converter that the value is unset
+            // Checks if the value provided is null, if so then tell the converter that the value is unset
             if (value == null)
                 return DependencyProperty.UnsetValue;
 
             // Gets the parameter and check if it is null, if so then tells the converter, that the value is unset
-            string enumValueName = parameter as string;
-            if (enumValueName == null)
+            Type parameterValue = parameter as Type;
+            if (parameterValue == null)
                 return DependencyProperty.UnsetValue;
 
-            // Then the parameter is converted to the enum type and returned
-            return (Visibility)value == Visibility.Visible ? Enum.Parse(targetType, enumValueName) : DependencyProperty.UnsetValue;
+            // Then the parameter is converted to the type and returned
+            return (Visibility)value == Visibility.Visible ? parameterValue : DependencyProperty.UnsetValue;
         }
 
         #endregion
