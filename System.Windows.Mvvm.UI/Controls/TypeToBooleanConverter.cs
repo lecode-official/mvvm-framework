decd5b1d53a7698d92519fa3169a5b158054f258
lukasroegner <lukas.roegner@outlook.com>
2016-03-26 13:59:58 +0100

    Added two new converters that check for type equality of the value and the converter parameter. The functionality is similar to the enum converters.

---
+++ System.Windows.Mvvm.UI/Controls/TypeToBooleanConverter.cs
@@ -0,0 +1,71 @@
+﻿
+#region Using Directives
+
+using System.Globalization;
+using System.Windows.Data;
+
+#endregion
+
+namespace System.Windows.Controls
+{
+    /// <summary>
+    /// Converts a type to a boolean value, if the parameter has the same value as the type.
+    /// </summary>
+    public class TypeToBooleanConverter : IValueConverter
+    {
+        #region IValueConverter Implementation
+
+        /// <summary>
+        /// Convertes the value parameter to a boolean value.
+        /// </summary>
+        /// <param name="value">The type that is to be converted.</param>
+        /// <param name="targetType">The type to which the value is to be converted. In this case it is always boolean.</param>
+        /// <param name="parameter">A parameter that determines the value to which the type has to be equal to return true.</param>
+        /// <param name="culture">The culture information of the current culture, so that parsing can be adjusted to cultural conventions.</param>
+        /// <returns>Returns true if the value parameter has the same value as the parameter parameter.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Checks if the value provided is null, if so then tell the converter that the value is unset
+            if (value == null)
+                return DependencyProperty.UnsetValue;
+
+            // Gets the value as type and check if it is null, if so then tells the converter, that the value is unset
+            Type typeValue = value as Type;
+            if (typeValue == null)
+                return DependencyProperty.UnsetValue;
+
+            // Gets the parameter and check if it is null, if so then tells the converter, that the value is unset
+            Type parameterValue = parameter as Type;
+            if (parameterValue == null)
+                return DependencyProperty.UnsetValue;
+
+            // If the parameter equals the value that is to be converted, then returns true, otherwise false is returned
+            return typeValue == parameterValue;
+        }
+
+        /// <summary>
+        /// Converts a boolean value back to a type.
+        /// </summary>
+        /// <param name="value">The value that is to be converted.</param>
+        /// <param name="targetType">The type to which the value is to be converted. In this case it is a type.</param>
+        /// <param name="parameter">A parameter whose type is returned.</param>
+        /// <param name="culture">The culture information of the current culture, so that parsing can be adjusted to cultural conventions.</param>
+        /// <returns>Returns the type of the parameter.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Checks if the value provided is null, if so then tell the converter that the value is unset
+            if (value == null)
+                return DependencyProperty.UnsetValue;
+            
+            // Gets the parameter and check if it is null, if so then tells the converter, that the value is unset
+            Type parameterValue = parameter as Type;
+            if (parameterValue == null)
+                return DependencyProperty.UnsetValue;
+
+            // Then the parameter is converted to the type and returned
+            return (bool)value ? parameterValue : DependencyProperty.UnsetValue;
+        }
+
+        #endregion
+    }
+}
