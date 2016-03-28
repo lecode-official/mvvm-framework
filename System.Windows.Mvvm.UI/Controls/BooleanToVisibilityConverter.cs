d5a74488c5ef18b063cfbddb314f553e9c90e059
David Neumann <david.neumann@live.com>
2016-03-28 23:14:36 +0200

    Added a custom boolean to visibility value converter, which, unlike the standard boolean to visibility converter, does not return DependencyProperty.UnsetValue, when the input value is null, but it returns Visibility.Collapsed. This is very helpful, when the binding is asynchronous, because in an asynchronous binding, the value can be undetermined (i.e. null) for a short amount of time, which results in the control to be displayed for a short period of time, even if the binding value eventually evaluates to false.

--- System.Windows.Mvvm.UI/Controls/BooleanToVisibilityConverter.cs
+++ System.Windows.Mvvm.UI/Controls/BooleanToVisibilityConverter.cs
@@ -9,15 +9,17 @@ using System.Windows.Data;
 namespace System.Windows.Controls
 {
     /// <summary>
-    /// Convertes boolean values into visibility values and vice versa. Unlike the standard boolean to visibility converter it inverts the boolean values, which
-    /// means, that false converts to visible and true converts to collapsed.
+    /// Convertes boolean values into visibility values and vice versa. Unlike the standard boolean to visibility converter it does not return
+    /// <c>DependencyProperty.UnsetValue</c>, when the input value is <c>null</c>, but it returns <c>Visibility.Collapsed</c>. This is very helpful, when the
+    /// binding is asynchronous, because in an asynchronous binding, the value can be undetermined (i.e. <c>null</c>) for a short amount of time, which results
+    /// in the control to be displayed for a short period of time, even if the binding value eventually evaluates to <c>false</c>.
     /// </summary>
-    public class InvertedBooleanToVisibilityConverter : IValueConverter
+    public class BooleanToVisibilityConverter : IValueConverter
     {
         #region IValueConverter Implementation
 
         /// <summary>
-        /// Convertes the boolean value parameter to a visibility value. True converts to collapsed and false converts to visible.
+        /// Convertes the boolean value parameter to a visibility value. True converts to visible and false converts to collapsed.
         /// </summary>
         /// <param name="value">The boolean value that is to be converted.</param>
         /// <param name="targetType">The type to which the value is to be converted. In this case it is always <see cref="Visibility"/>.</param>
@@ -27,19 +29,19 @@ namespace System.Windows.Controls
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value == null)
-                return Visibility.Visible;
-            return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+                return Visibility.Collapsed;
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
         }
 
         /// <summary>
-        /// Convertes the visibility value parameter to a boolean value. Collapsed converts to true and visible converts to false.
+        /// Convertes the visibility value parameter to a boolean value. Visible converts to true and collapsed converts to false.
         /// </summary>
         /// <param name="value">The visibility value that is to be converted.</param>
         /// <param name="targetType">The type to which the value is to be converted. In this case it is always <see cref="bool"/>.</param>
         /// <param name="parameter">A parameter for the conversion. Not used in this converter, so it should always be null.</param>
         /// <param name="culture">The culture information of the current culture, so that parsing can be adjusted to cultural conventions.</param>
         /// <returns>Returns false if the value is <see cref="Visible"/> and true if the value is <see cref="Collapsed"/>.</returns>
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => (Visibility)value == Visibility.Visible ? false : true;
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => (Visibility)value == Visibility.Visible ? true : false;
 
         #endregion
     }
