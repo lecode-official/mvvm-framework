30e1d3dac146ede9a4001f9b4acdfa5cd8a46b80
David Neumann <david.neumann@lecode.de>
2016-04-18 16:47:15 +0200

    Created some general classes for the navigation service.

---
+++ UWP/Windows.Mvvm.Services.Navigation/IViewModel.cs
@@ -0,0 +1,53 @@
+﻿
+#region Using Directives
+
+using System;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace Windows.Mvvm.Services.Navigation
+{
+    /// <summary>
+    /// Represents the interface which is implemented by all view models.
+    /// </summary>
+    public interface IViewModel : IDisposable
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets a value that determine whether the view of this view model is currently displayed.
+        /// </summary>
+        bool IsInView { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Is called when the view model is created (before the user is navigated to the view and before the OnNavigateToAsync event is called). After the view model was created, it is cached and reused until it is destroyed, therefore OnActivateAsync
+        /// is only called once in the life cycle of a view model.
+        /// </summary>
+        Task OnActivateAsync();
+
+        /// <summary>
+        /// Is called before the view model is navigated to. Other than OnActivateAsync, OnNavigateToAsync is called everytime the user navigates to this view model.
+        /// </summary>
+        /// <param name="e">The event arguments, that allows the navigation to be cancelled.</param>
+        Task OnNavigateToAsync(NavigationEventArgs e);
+
+        /// <summary>
+        /// Is called before the view model is navigated away from. Other than OnDeactivateAsync, OnNavigateFromAsync is called everytime the user navigates away from this view model.
+        /// </summary>
+        /// <param name="e">The event arguments, that allows the navigation to be cancelled.</param>
+        Task OnNavigateFromAsync(NavigationEventArgs e);
+
+        /// <summary>
+        /// Is called when the view model gets deactivated. The view model only gets deactivated when the navigation stack of the window, that contains the view of this view model, is cleared, or when the windows, containing the view of
+        /// this view model, is closed. Therefore OnDeactivateAsync is only called once in the lifecycle of a view model.
+        /// </summary>
+        Task OnDeactivateAsync();
+
+        #endregion
+    }
+}
---
+++ UWP/Windows.Mvvm.Services.Navigation/NavigationResult.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Windows.Mvvm.Services.Navigation
+{
+    /// <summary>
+    /// Represents an enumeration that contains the result value of a navigation.
+    /// </summary>
+    public enum NavigationResult
+    {
+        /// <summary>
+        /// The navigation was cancelled by the view model.
+        /// </summary>
+        Canceled,
+
+        /// <summary>
+        /// The navigation was successful.
+        /// </summary>
+        Navigated
+    }
+}
