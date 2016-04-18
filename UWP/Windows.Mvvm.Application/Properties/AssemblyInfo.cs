ececb70beaf83da0da56818a4e8fd1d3da839dd0
David Neumann <david.neumann@live.com>
2016-04-18 14:46:59 +0200

    Created the first library.

---
+++ UWP/Windows.Mvvm.Application/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿
+#region 
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+#region General Assembly Information
+
+[assembly: AssemblyTitle("MVVM Framework Application")]
+[assembly: AssemblyDescription("The MVVM Framework is a compact and simple, yet powerful implementation of the MVVM pattern. This part of the MVVM Framework contains a base class for an application.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVVM Framework")]
+[assembly: AssemblyCopyright("Copyright © David Neumann & Lukas Rögner 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+#endregion
+
+#region Assembly Version
+
+[assembly: AssemblyVersion("0.2.7.*")]
+[assembly: AssemblyFileVersion("0.2.7.0")]
+
+#endregion
