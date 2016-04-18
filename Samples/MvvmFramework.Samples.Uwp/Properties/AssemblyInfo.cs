57194fb59795988837cdfaaa15a676b9ebd29c87
David Neumann <david.neumann@lecode.de>
2016-04-18 14:41:51 +0200

    Fixed some NuGet package path hint problems, which caused two projects to not build in debug. Also added a new UWP sample app, which will be used to develop an UWP version of the MVVM Framework.

--- Samples/MvvmFramework.Samples.Uwp/Properties/AssemblyInfo.cs
+++ Samples/MvvmFramework.Samples.Uwp/Properties/AssemblyInfo.cs
@@ -1,31 +1,22 @@
 ﻿
 #region Using Directives
 
-using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 #endregion
 
 #region General Assembly Information
 
-[assembly: AssemblyTitle("MVVM Framework Sample Application (WPF)")]
-[assembly: AssemblyDescription("The MVVM Framework is a compact and simple, yet powerful implementation of the MVVM pattern. This is a sample WPF application, which demonstrates how to use the basic functionality of the framework.")]
+[assembly: AssemblyTitle("MVVM Framework Sample Application (UWP)")]
+[assembly: AssemblyDescription("The MVVM Framework is a compact and simple, yet powerful implementation of the MVVM pattern. This is a sample UWP application, which demonstrates how to use the basic functionality of the framework.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MVVM Framework")]
 [assembly: AssemblyCopyright("Copyright © David Neumann & Lukas Rögner 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
-
-#endregion
-
-#region WPF Information
-
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 #endregion
 
