24b5e92e838c28d5b1728a4a3e5840c2524337e3
David Neumann <david.neumann@live.com>
2016-04-18 14:41:51 +0200

    Fixed some NuGet package path hint problems, which caused two projects to not build in debug. Also added a new UWP sample app, which will be used to develop an UWP version of the MVVM Framework.

---
+++ Samples/MvvmFramework.Samples.Uwp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+#region Using Directives
+
+using Windows.UI.Xaml.Controls;
+
+#endregion
+
+namespace MvvmFramework.Samples.Uwp
+{
+    /// <summary>
+    /// Represents the main view of the application.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new <see cref="MainPage"/> instance.
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
