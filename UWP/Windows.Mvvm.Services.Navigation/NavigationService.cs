630a6af1c15281748b8f197d352fd8d925549621
David Neumann <david.neumann@live.com>
2016-04-18 19:32:26 +0200

    Started to work on the window navigation service and the navigation service.

---
+++ UWP/Windows.Mvvm.Services.Navigation/NavigationService.cs
@@ -0,0 +1,86 @@
+﻿
+#region Using Directives
+
+using System;
+using System.InversionOfControl.Abstractions;
+using System.Threading.Tasks;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+
+#endregion
+
+namespace Windows.Mvvm.Services.Navigation
+{
+    /// <summary>
+    /// Represents a manager, that manages the navigation of a views within a window.
+    /// </summary>
+    public class NavigationService
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new <see cref="NavigationService"/> instance.
+        /// </summary>
+        /// <param name="iocContainer">The IOC container which is used to instantiate the views and their corresponding view models.</param>
+        /// <param name="navigationFrame">The frame with in which the actual navigation between views takes place.</param>
+        internal NavigationService(IReadOnlyIocContainer iocContainer, Frame navigationFrame)
+        {
+            // Validates the arguments
+            if (iocContainer == null)
+                throw new ArgumentNullException(nameof(iocContainer));
+
+            // Gets the application view, which represents the current window
+            this.Window = ApplicationView.GetForCurrentView();
+
+            // Stores the IoC container and the frame for later use
+            this.iocContainer = iocContainer;
+            this.navigationFrame = navigationFrame;
+        }
+
+        #endregion
+
+        #region Private Fields
+
+        /// <summary>
+        /// Contains the IOC container which is used to instantiate the views and their corresponding view models.
+        /// </summary>
+        private readonly IReadOnlyIocContainer iocContainer;
+
+        /// <summary>
+        /// Contains the frame with in which the actual navigation between views takes place.
+        /// </summary>
+        private readonly Frame navigationFrame;
+
+        #endregion
+
+        #region Internal Properties
+
+        /// <summary>
+        /// Gets or sets the view model of the view that is currently in view of the window that is being managed by this navigation service.
+        /// </summary>
+        internal IViewModel CurrentViewModel { get; set; }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the application view, which represents the window, managed by the navigation service.
+        /// </summary>
+        public ApplicationView Window { get; private set; }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Closes the window that is managed by this navigation service.
+        /// </summary>
+        public Task CloseWindowAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ UWP/Windows.Mvvm.Services.Navigation/WindowNavigationResult.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Windows.Mvvm.Services.Navigation
+{
+    /// <summary>
+    /// Represents a wrapper object, which encapsulates the result of creating a new window and navigating to it.
+    /// </summary>
+    public class WindowNavigationResult
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the result of the navigation (e.g. the navigation was cancelled).
+        /// </summary>
+        public NavigationResult Result { get; set; }
+
+        /// <summary>
+        /// Gets or sets the navigation service for the newly created window. May be <c>null</c> if the window does not support navigation or the navigation was cancelled.
+        /// </summary>
+        public NavigationService NavigationService { get; set; }
+
+        #endregion
+    }
+}
