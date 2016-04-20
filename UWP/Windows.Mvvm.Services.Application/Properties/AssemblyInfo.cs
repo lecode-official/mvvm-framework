9b97a2b701417f031710993d19816df47dc19406
David Neumann <david.neumann@live.com>
2016-04-20 17:15:24 +0200

    Integrated the first draft of the application service for UWP.

---
+++ UWP/Windows.Mvvm.Services.Application/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿
+#region Using Directives
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+#region General Assembly Information
+
+[assembly: AssemblyTitle("MVVM Framework Application Service")]
+[assembly: AssemblyDescription("The MVVM Framework is a compact and simple, yet powerful implementation of the MVVM pattern. This service offers several application-specific methods.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("MVVM Framework")]
+[assembly: AssemblyCopyright("Copyright © David Neumann & Lukas Rögner 2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+#endregion
+
+#region Assembly Version
+
+[assembly: AssemblyVersion("0.2.8.*")]
+[assembly: AssemblyFileVersion("0.2.8.0")]
+
+#endregion
