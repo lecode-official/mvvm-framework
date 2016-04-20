527ab5e34e63014ca194ff80a9e0c924a32a9b52
David Neumann <david.neumann@live.com>
2016-04-20 21:33:36 +0200

    Created the first draft of the dialog service.

---
+++ UWP/Windows.Mvvm.Services.Dialog/DialogResult.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Windows.Mvvm.Services.Dialog
+{
+    /// <summary>
+    /// The result of a dialog.
+    /// </summary>
+    public enum DialogResult
+    {
+        /// <summary>
+        /// The dialog had no result.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// The user clicked okay.
+        /// </summary>
+        Okay = 1,
+
+        /// <summary>
+        /// The user canceled the dialog.
+        /// </summary>
+        Cancel = 2,
+
+        /// <summary>
+        /// The user clicked yes.
+        /// </summary>
+        Yes = 6,
+
+        /// <summary>
+        /// The user clicked no.
+        /// </summary>
+        No = 7
+    }
+}
---
+++ UWP/Windows.Mvvm.Services.Dialog/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿
+#region Using Directives
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+#region General Assembly Information
+
+[assembly: AssemblyTitle("MVVM Framework Dialog Service")]
+[assembly: AssemblyDescription("The MVVM Framework is a compact and simple, yet powerful implementation of the MVVM pattern. This service offers access to message boxes and file dialogs.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVVM Framework")]
+[assembly: AssemblyCopyright("Copyright © David Neumann & Lukas Rögner 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+#endregion
+
+#region Assembly Version
+
+[assembly: AssemblyVersion("0.2.8.*")]
+[assembly: AssemblyFileVersion("0.2.8.0")]
+
+#endregion
