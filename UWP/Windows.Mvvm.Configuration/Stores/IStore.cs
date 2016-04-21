95b7238a33aba2a1addae4fc3470c8c567d073dd
David Neumann <david.neumann@lecode.de>
2016-04-21 19:21:12 +0200

    Started to work on the configuration project for UWP.

---
+++ UWP/Windows.Mvvm.Configuration/Stores/IStore.cs
@@ -0,0 +1,31 @@
+﻿
+#region Using Directives
+
+using System.Threading.Tasks;
+
+#endregion
+
+namespace Windows.Mvvm.Configuration.Stores
+{
+    /// <summary>
+    /// Represents the interface for the storage method of the configuration data.
+    /// </summary>
+    public interface IStore
+    {
+        #region Methods
+
+        /// <summary>
+        /// Loads the stores configuration data and returns them as serialized string.
+        /// </summary>
+        /// <returns>Returns the loaded data.</returns>
+        Task<string> LoadAsync();
+
+        /// <summary>
+        /// Stores the configuration data.
+        /// </summary>
+        /// <param name="content">The serializes configuration data.</param>
+        Task StoreAsync(string content);
+
+        #endregion
+    }
+}
