00ddd374dfc856cd16130a9cf943c2af183a2e4b
David Neumann <david.neumann@live.com>
2016-04-27 18:15:26 +0200

    Adapted the WPF sample to use all the new features.

--- Samples/MvvmFramework.Samples.Wpf/ViewModels/TodoListItemViewModel.cs
+++ Samples/MvvmFramework.Samples.Wpf/ViewModels/TodoListItemViewModel.cs
@@ -1,7 +1,8 @@
 ﻿
 #region Using Directives
 
-using ReactiveUI;
+using System.Windows.Mvvm.Reactive;
+using System.Windows.Mvvm.Sample.Models;
 
 #endregion
 
@@ -10,93 +11,45 @@ namespace System.Windows.Mvvm.Sample.ViewModels
     /// <summary>
     /// Represents a view model for a single todo list item.
     /// </summary>
-    public class TodoListItemViewModel : ReactiveObject
+    public class TodoListItemViewModel
     {
-        #region Public Properties
+        #region Constructors
 
         /// <summary>
-        /// Contains the unique identifier of the todo list item, which is a global unique ID.
+        /// Initializes a new <see cref="TodoListItemViewModel"/> instance.
         /// </summary>
-        private string id;
+        /// <param name="todoListItem">The model from which the view model is to be generated.</param>
+        public TodoListItemViewModel(TodoListItem todoListItem)
+        {
+            this.Id.Value = todoListItem.Id;
+            this.Title.Value = todoListItem.Title;
+            this.Description.Value = todoListItem.Description;
+            this.IsFinished.Value = todoListItem.IsFinished;
+        }
+
+        #endregion
+
+        #region Public Properties
 
         /// <summary>
         /// Gets or sets the unique identifier of the todo list item, which is a global unique ID.
         /// </summary>
-        public string Id
-        {
-            get
-            {
-                return this.id;
-            }
-
-            set
-            {
-                this.RaiseAndSetIfChanged(ref this.id, value);
-            }
-        }
-
-        /// <summary>
-        /// Contains the title of the todo list item.
-        /// </summary>
-        private string title;
+        public ReactiveProperty<string> Id { get; } = new ReactiveProperty<string>();
 
         /// <summary>
         /// Gets or sets the title of the todo list item.
         /// </summary>
-        public string Title
-        {
-            get
-            {
-                return this.title;
-            }
-
-            set
-            {
-                this.RaiseAndSetIfChanged(ref this.title, value);
-            }
-        }
-
-        /// <summary>
-        /// Contains a detailed description of the todo list item, which contains the steps to finish it.
-        /// </summary>
-        private string description;
-
+        public ReactiveProperty<string> Title { get; } = new ReactiveProperty<string>();
+        
         /// <summary>
         /// Gets or sets a detailed description of the todo list item, which contains the steps to finish it.
         /// </summary>
-        public string Description
-        {
-            get
-            {
-                return this.description;
-            }
-
-            set
-            {
-                this.RaiseAndSetIfChanged(ref this.description, value);
-            }
-        }
-
-        /// <summary>
-        /// Contains a value that determines whether the todo list item has already been finished an can therefore be removed from the todo list.
-        /// </summary>
-        private bool isFinished;
+        public ReactiveProperty<string> Description { get; } = new ReactiveProperty<string>();
 
         /// <summary>
         /// Gets or sets a value that determines whether the todo list item has already been finished an can therefore be removed from the todo list.
         /// </summary>
-        public bool IsFinished
-        {
-            get
-            {
-                return this.isFinished;
-            }
-
-            set
-            {
-                this.RaiseAndSetIfChanged(ref this.isFinished, value);
-            }
-        }
+        public ReactiveProperty<bool> IsFinished { get; } = new ReactiveProperty<bool>();
 
         #endregion
     }
