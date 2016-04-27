8ab9a27844d148f2ad1980b219f6d7067d2e1998
David Neumann <david.neumann@live.com>
2016-04-27 18:19:51 +0200

    Ported the custom reactive command and reactive property over to the UWP implementation of the framework.

---
+++ UWP/Windows.Mvvm.Reactive/ReactiveCommand.cs
@@ -0,0 +1,69 @@
+﻿
+#region Using Directives
+
+using System;
+using System.Reactive;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace Windows.Mvvm.Reactive
+{
+    /// <summary>
+    /// Represents a reactive implementation of command.
+    /// </summary>
+    public class ReactiveCommand : ReactiveCommand<object, Unit>
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new <see cref="ReactiveCommand"/> instance.
+        /// </summary>
+        /// <param name="execute">The delegate that is to be executed when the command is being executed.</param>
+        /// <param name="canExecute">An observable, which determines whether the command can be executed or not. If <c>null</c> is specified, then the constructor defaults to the <see cref="IsExecuting"/> observable.</param>
+        /// <param name="blockOnExecution">Determines whether can execute always returns false while the command is being executed.</param>
+        public ReactiveCommand(Func<Task> execute, IObservable<bool> canExecute, bool blockOnExecution)
+            : base(async (parameter) =>
+            {
+                await execute();
+                return Unit.Default;
+            }, canExecute, blockOnExecution)
+        { }
+
+        /// <summary>
+        /// Initializes a new <see cref="ReactiveCommand"/> instance.
+        /// </summary>
+        /// <param name="execute">The delegate that is to be executed when the command is being executed.</param>
+        /// <param name="canExecute">An observable, which determines whether the command can be executed or not. If <c>null</c> is specified, then the constructor defaults to the <see cref="IsExecuting"/> observable.</param>
+        public ReactiveCommand(Func<Task> execute, IObservable<bool> canExecute)
+            : base(async parameter =>
+            {
+                await execute();
+                return Unit.Default;
+            }, canExecute)
+        { }
+
+        /// <summary>
+        /// Initializes a new <see cref="ReactiveCommand"/> instance.
+        /// </summary>
+        /// <param name="execute">The delegate that is to be executed when the command is being executed.</param>
+        public ReactiveCommand(Func<Task> execute)
+            : base(async parameter =>
+            {
+                await execute();
+                return Unit.Default;
+            })
+        { }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Executes the command.
+        /// </summary>
+        public Task ExecuteAsync() => base.ExecuteAsync(null);
+
+        #endregion
+    }
+}
