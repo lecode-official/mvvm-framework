a59ef80c4c3999cd38c0b0001dce045b9e701d8e
David Neumann <david.neumann@lecode.de>
2016-05-12 11:28:31 +0200

    Cleaned up the code a little bit.

--- UWP/Windows.Mvvm.Reactive/DerivedReactiveCollection.cs
+++ UWP/Windows.Mvvm.Reactive/DerivedReactiveCollection.cs
@@ -2,10 +2,8 @@
 #region Using Directives
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 
